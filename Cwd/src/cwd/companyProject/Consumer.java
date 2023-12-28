3555d6e5ab33aac049fe6584d36e5b0e4d783393
SurajBhalerao27 <154430492+SurajBhalerao27@users.noreply.github.com>
2023-12-28 19:49:41 +0530

    Add files via upload
    first commit for the java programs

---
+++ Cwd/src/cwd/companyProject/Consumer.java
@@ -0,0 +1,20 @@
+package cwd.companyProject;
+
+public class Consumer extends Thread {
+	Company c;
+	Consumer(Company c) {
+		this.c = c;
+	}
+	public void run() {
+
+		while (true) {
+
+			this.c.consume_item( );
+			try {
+				Thread.sleep(5000);
+			} catch (Exception e) {
+				System.out.println(e);
+			}
+		}
+	}
+}
---
+++ Cwd/src/cwd/companyProject/Producer.java
@@ -0,0 +1,23 @@
+package cwd.companyProject;
+
+public class Producer extends Thread {
+	Company c;
+
+	Producer(Company c) {
+		this.c = c;
+	}
+
+	public void run() {
+		int i = 1;
+		while (true) {
+			this.c.produce_item(i);
+			try {
+				Thread.sleep(1000);
+			} catch (Exception e) {
+				System.out.println(e);
+			}
+			i++;
+
+		}
+	}
+}
