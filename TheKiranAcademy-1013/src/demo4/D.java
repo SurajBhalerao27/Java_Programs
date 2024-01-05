2bfea44a367628cdc0616da1cb017543a411f28b
SurajBhalerao27 <154430492+SurajBhalerao27@users.noreply.github.com>
2024-01-05 22:50:45 +0530

    initial commit

---
+++ TheKiranAcademy-1013/src/demo4/D.java
@@ -0,0 +1,14 @@
+package demo4;
+
+public class D {
+	public int evenOrOdd(int n) {
+
+        if(n % 2 == 0)
+            System.out.println(n + " is even");
+        else
+            System.out.println(n + " is odd");
+		return n; 
+		
+	}
+
+}
